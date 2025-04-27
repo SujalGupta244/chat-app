aa44c84a28efead6b349a92a89474117ee8f2316
Sujal Gupta <sujalgupta5656@gmail.com>
2025-04-27 19:05:24 +0530

    making project production ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,13 @@ import { connectDB } from "./lib/db.js"
 import cookieParser from 'cookie-parser'
 import cors from "cors"
 import { app, server } from "./lib/socket.js"
+import path from "path"
 
 dotenv.config()
 // const app = express()
 const PORT = process.env.PORT || 5000
+
+const __dirname = path.resolve();
 
 app.use(cors({
     origin: "http://localhost:5173",
@@ -20,6 +23,15 @@ app.use(express.json({ limit: '50mb' }))
 
 app.use("/api/auth" , authRoutes)
 app.use("/api/messages" , messageRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req,res) =>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
+
 
 server.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
