904aa60530ae6298aa5085f52ad56eb4bc45aac1
Sujal Gupta <sujalgupta5656@gmail.com>
2026-03-23 17:28:01 +0530

    updated prisma version

--- backend/src/db/prisma.ts
+++ backend/src/db/prisma.ts
@@ -1,5 +1,10 @@
-import {PrismaClient} from '@prisma/client'
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const prisma = new PrismaClient()
+const connectionString = process.env.DATABASE_URL!;
 
-export default prisma
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
